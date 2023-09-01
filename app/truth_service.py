7c84fb41d6ebc9e96d442c9d49996dabd269bfb2
MJ Rossetti <s2t2@users.noreply.github.com>
2023-09-01 14:08:00 -0400

    Fetch user timeline

--- app/truth_service.py
+++ app/truth_service.py
@@ -1,6 +1,7 @@
 
 import os
 from pprint import pprint
+from datetime import date, timedelta
 
 from truthbrush import Api
 from dotenv import load_dotenv
@@ -10,12 +11,37 @@ load_dotenv()
 TRUTH_USERNAME = os.getenv("TRUTH_USERNAME")
 TRUTH_PASSWORD = os.getenv("TRUTH_PASSWORD")
 
+BEGINNING_OF_TIME = date(2022, 1, 1) # start of timeline
+
+
+
+class TruthService:
+    def __init__(self, username=TRUTH_USERNAME, password=TRUTH_PASSWORD):
+        self.username = username
+        self.password = password
+
+        self.client = Api(username=username, password=password)
+
+
+    def get_user(self, handle='realDonaldTrump'):
+        return self.client.lookup(user_handle=handle)
+
+    def get_user_timeline(self, handle='realDonaldTrump', created_after=BEGINNING_OF_TIME):
+        """returns a generator"""
+        return self.client.pull_statuses(username=handle, created_after=created_after, replies=True)
+
+
 
 if __name__ == "__main__":
 
-    username=TRUTH_USERNAME
-    password=TRUTH_PASSWORD
-    client = Api(username=username, password=password)
 
-    user = client.lookup(user_handle='realDonaldTrump')
+    service = TruthService()
+    user = service.get_user()
     pprint(user)
+
+    last_week = date.today() - timedelta(days=7)
+    timeline = list(service.get_user_timeline(created_after=last_week))
+    print(len(timeline))
+    pprint(timeline[0])
+    #for tweet in timeline:
+    #    print(tweet.id)

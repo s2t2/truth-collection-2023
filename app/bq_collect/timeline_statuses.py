ffed62de0da80a9706eaf9ce0a16912b293d7b8a
MJ Rossetti <s2t2@users.noreply.github.com>
2023-09-04 14:13:42 -0400

    Better logging for threaded runs

--- app/bq_collect/timeline_statuses.py
+++ app/bq_collect/timeline_statuses.py
@@ -8,9 +8,6 @@ def update_timeline_statuses(username=COLLECTION_USERNAME, bq=None, truth=None, 
     bq = bq or BigQueryService()
     truth = truth or TruthService()
 
-    print("------------")
-    print(f"FETCHING STATUSES FOR '{username}'...")
-
     if not since_id:
         # what is the latest status we have for this person?
         sql = f"""
@@ -25,23 +22,24 @@ def update_timeline_statuses(username=COLLECTION_USERNAME, bq=None, truth=None, 
             since_id = results[0]["status_id"]
             #since = results[0]["created_at"]
 
-    print("SINCE:", since_id)
+    print("------------")
+    print(f"FETCHING STATUSES FOR '{username}'", "SINCE:", since_id, "...")
     timeline = truth.get_user_timeline(username=username, since_id=since_id, verbose=verbose)
-    #timeline = sorted(timeline, key=lambda post: post["id"])
+
     records = []
     collected_at = generate_timestamp() # using the same collection time for each run may help identify collection runs later
     for status in timeline:
         record = parse_status(status)
         record["collected_at"] = collected_at # consider adding this to the parse_status method
         records.append(record)
-    print("FETCHED:", len(records))
+    print("FETCHED:", len(records), f"FOR '{username}'")
 
     if any(records):
-        print("SAVING STATUSES...")
+        print("SAVING...")
         table = bq.timeline_statuses_table # api call table reference. after initial migration table ref might not yet be available, so we cound consider check here first before going through the trouble of fetching the timeline
         errors = bq.insert_records_in_batches(table, records)
         if any(errors):
-            print("ERRORS:")
+            print("ERRORS:", f"FOR '{username}'")
             print(errors)
 
 

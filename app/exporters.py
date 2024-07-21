379eff7fcf83a296aeb3a736fa9952cb204fa93e
MJ Rossetti <s2t2@users.noreply.github.com>
2024-07-21 16:36:41 -0400

    Export posts to SQLite and CSV

---
+++ app/exporters.py
@@ -0,0 +1,119 @@
+
+import os
+import sqlite3
+from contextlib import contextmanager
+from pandas import DataFrame
+
+EXPORTS_DIR = os.path.join(os.path.dirname(__file__), "..", "exports")
+
+DEFAULT_FILEPATH = os.path.join(EXPORTS_DIR, "truth_collection_development.sqlite")
+DB_FILEPATH = os.getenv("DB_FILEPATH") or DEFAULT_FILEPATH
+
+DESTRUCTIVE_MODE = bool(os.getenv("DESTRUCTIVE") == "true")
+
+#def to_table(df:DataFrame, table_name:str):
+#    connection = connect(DB_FILEPATH)
+#    df.to_sql(table_name, connection, if_exists='replace', index=False)
+#    connection.close()
+
+
+#@contextmanager
+#def manage_connection(db_filepath):
+#    connection = sqlite3.connect(db_filepath)
+#    try:
+#        yield connection
+#    finally:
+#        connection.close()
+#
+#def with_connection(func):
+#    def wrapper(*args, **kwargs):
+#        with manage_connection(DB_FILEPATH) as connection:
+#            return func(*args, connection=connection, **kwargs)
+#    return wrapper
+#
+#@with_connection
+#def to_table(df:DataFrame, table_name:str, connection):
+#    df.to_sql(table_name, connection, if_exists='replace', index=False)
+
+
+class Database:
+
+    def __init__(self, filepath=DB_FILEPATH, destructive=False):
+        """A base interface into SQLite database.
+
+        Params:
+            filepath (str):
+                path to the database that exists or will be created
+        """
+        self.destructive = bool(destructive)
+
+        self.filepath = filepath
+        print("------------------")
+        print("DB FILEPATH:", os.path.abspath(self.filepath))
+
+        self.connection = sqlite3.connect(self.filepath)
+        self.connection.row_factory = sqlite3.Row
+        #print("CONNECTION:", self.connection)
+
+        self.cursor = self.connection.cursor()
+        #print("CURSOR", self.cursor)
+
+        if self.destructive:
+            print("DB DESTRUCTIVE:", self.destructive)
+            #self.drop_tables()
+            if os.path.exists(self.filepath):
+                os.remove(self.filepath)
+
+    #def drop_table(self, table_name):
+    #    self.cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+
+    def insert_data(self, table_name:str, records:list):
+        """Params:
+            table_name (str) : name of table to insert data into
+            records (list[dict]) : records to save
+        """
+        if not records or not any(records):
+            return None
+
+        df = DataFrame(records)
+        self.insert_df(df=df, table_name=table_name)
+
+    def insert_df(self, table_name:str, df:DataFrame):
+        """Params:
+
+            table_name (str):
+                name of table to insert data into
+
+            df (pandas.DataFrame):
+                dataframe of records to save
+        """
+        #df.index.rename("row_id", inplace=True) # assigns a column label "id" for the index column
+        #df.index += 1 # starts ids at 1 instead of 0
+
+        # convert lists to CSV strings (b/c SQLite can't handle nested data)
+        for column in df.columns:
+            if df[column].apply(lambda x: isinstance(x, list)).any():
+                df[column] = df[column].apply(lambda x: ', '.join(x) if isinstance(x, list) else x)
+
+        # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_sql.html
+        df.to_sql(table_name, con=self.connection,
+            if_exists="append", # append to existing tables (don't throw error)
+            #index_label="row_id", # store unique ids for the rows, so we could count them (JK this restarts numbering at 1 for each df)
+            index=False
+        )
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    df = DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+
+    print(df.head())
+    print("INSERTING DATA...")
+
+    db = Database()
+    db.insert_df(df=df, table_name="test_exports")
--- app/truth_service.py
+++ app/truth_service.py
@@ -1,7 +1,8 @@
 
 import os
 from pprint import pprint
-from datetime import date, timedelta, datetime, timezone
+from datetime import timedelta, datetime, timezone
+from dateutil import parser as date_parser
 
 from truthbrush import Api
 from dotenv import load_dotenv
@@ -13,6 +14,7 @@ load_dotenv()
 TRUTH_USERNAME = os.getenv("TRUTH_USERNAME")
 TRUTH_PASSWORD = os.getenv("TRUTH_PASSWORD")
 COLLECTION_USERNAME= os.getenv("COLLECTION_USERNAME", default="realDonaldTrump")
+VERBOSE_MODE = bool(os.getenv("VERBOSE_MODE") == "true")
 
 class TruthService:
     def __init__(self, username=TRUTH_USERNAME, password=TRUTH_PASSWORD):
@@ -21,104 +23,140 @@ class TruthService:
     def get_user(self, username=COLLECTION_USERNAME):
         return self.client.lookup(user_handle=username)
 
-    def get_user_timeline(self, username=COLLECTION_USERNAME, replies=True, verbose=False, since_id=None, created_after=None):
+    def get_user_timeline(self, username=COLLECTION_USERNAME, replies=True, verbose=VERBOSE_MODE, since_id=None, created_after=None):
+        print(verbose)
         return self.client.pull_statuses(username=username, replies=replies, verbose=verbose, since_id=since_id, created_after=created_after)
 
+    @staticmethod
+    def parse_status(status):
+        # PARSER FUNCTIONS (FOR CONVERTING RAW DATA INTO DATABASE RECORDS):
+
+        #breakpoint()
+
+        # convert into timestamp format bigquery likes
+        created_at = status["created_at"] #> # '2023-09-01T14:39:30.235Z'
+        created_at = datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ') #> datetime.datetime(2023, 9, 1, 14, 39, 30, 235000)
+        created_at = generate_timestamp(created_at) #> '2023-09-01 14:39:30'
+
+        try:
+            group_id = status["group"]["id"]
+            group_slug = status["group"]["slug"]
+        except (KeyError, TypeError):
+            # most don't have a group (only status is if posted to a group)
+            group_id, group_slug = None, None
+
+        try:
+            reply_status_id = status["in_reply_to"]["id"]
+            reply_user_id = status["in_reply_to_account_id"]
+        except (KeyError, TypeError):
+            # most are not replies
+            reply_status_id, reply_user_id = None, None
+
+        try:
+            # ~78% have 0, ~21% have 1, <1% have more
+            # ... so we are only storing at most one media attachment
+            media_id = status["media_attachments"][0]["id"]
+            media_type = status["media_attachments"][0]["type"]
+            media_url = status["media_attachments"][0]["url"]
+        except (KeyError, TypeError, IndexError):
+            media_type, media_url, media_id = None, None, None
+
+        try:
+            # 92% have no mentions, 8% have one, <1% have multiple
+            # ... so we are only storing at most one mention
+            mention_id = status["mentions"][0]["id"]
+            mention_username = status["mentions"][0]["username"]
+            # FYI: the id of the mentioned user is not available
+        except (KeyError, TypeError, IndexError):
+            mention_id, mention_username = None, None
+
+        if any(status["tags"]):
+            # 99% have no tags, <1% have tags
+            # ... they look like this: {'name': 'Agenda47'}
+            tags = [t["name"] for t in status["tags"]]
+        else:
+            tags = None
+
+        return {
+            "status_id": status["id"],
+            "user_id": status["account"]["id"],
+            "username": status["account"]["username"],
+            "created_at": created_at,
+            "lang": status["language"],
+            "content": status["content"],
+
+            "group_id": group_id,
+            "group_slug": group_slug,
+
+            "reply_status_id": reply_status_id,
+            "reply_user_id": reply_user_id,
+
+            "media_id": media_id,
+            "media_type": media_type,
+            "media_url": media_url,
+
+            "mention_id": mention_id,
+            "mention_username": mention_username,
+
+            "tags": tags
+
+        }
 
 
-# PARSER FUNCTIONS (FOR CONVERTING RAW DATA INTO DATABASE RECORDS):
 
-def parse_status(status):
 
-    # convert into timestamp format bigquery likes
-    created_at = status["created_at"] #> # '2023-09-01T14:39:30.235Z'
-    created_at = datetime.strptime(created_at, '%Y-%m-%dT%H:%M:%S.%fZ') #> datetime.datetime(2023, 9, 1, 14, 39, 30, 235000)
-    created_at = generate_timestamp(created_at) #> '2023-09-01 14:39:30'
+def to_utc(date_str):
+    """Datetime formatter function. Ensures timezone is UTC."""
+    if not isinstance(date_str, str):
+        date_str = str(date_str) # handle datetime objects
+    return date_parser.parse(date_str).replace(tzinfo=timezone.utc)
 
-    try:
-        group_id = status["group"]["id"]
-        group_slug = status["group"]["slug"]
-    except (KeyError, TypeError):
-        # most don't have a group (only status is if posted to a group)
-        group_id, group_slug = None, None
 
-    try:
-        reply_status_id = status["in_reply_to"]["id"]
-        reply_user_id = status["in_reply_to_account_id"]
-    except (KeyError, TypeError):
-        # most are not replies
-        reply_status_id, reply_user_id = None, None
 
-    try:
-        # ~78% have 0, ~21% have 1, <1% have more
-        # ... so we are only storing at most one media attachment
-        media_type = status["media_attachments"][0]["type"]
-        media_url = status["media_attachments"][0]["url"]
-    except (KeyError, TypeError, IndexError):
-        media_type, media_url = None, None
-
-    try:
-        # 92% have no mentions, 8% have one, <1% have multiple
-        # ... so we are only storing at most one mention
-        mention_id = status["mentions"][0]["id"]
-        mention_username = status["mentions"][0]["username"]
-        # FYI: the id of the mentioned user is not available
-    except (KeyError, TypeError, IndexError):
-        mention_id, mention_username = None, None
-
-    if any(status["tags"]):
-        # 99% have no tags, <1% have tags
-        # ... they look like this: {'name': 'Agenda47'}
-        tags = [t["name"] for t in status["tags"]]
-    else:
-        tags = None
-
-    return {
-        "status_id": status["id"],
-        "user_id": status["account"]["id"],
-        "username": status["account"]["username"],
-        "created_at": created_at,
-        "lang": status["language"],
-        "content": status["content"],
-
-        "group_id": group_id,
-        "group_slug": group_slug,
-
-        "reply_status_id": reply_status_id,
-        "reply_user_id": reply_user_id,
-
-        "media_type": media_type,
-        "media_url": media_url,
-
-        "mention_id": mention_id,
-        "mention_username": mention_username,
-
-        "tags": tags
-
-    }
 
 
 
 
 if __name__ == "__main__":
 
+    from pandas import DataFrame
+    from app.exporters import EXPORTS_DIR, Database
+
     service = TruthService()
-    user = service.get_user()
+
+    #print("----------")
+    #print("USER:")
+    #user = service.get_user()
     #pprint(user)
-    print(user.keys())
 
-    recent_day = date.today() - timedelta(days=3)
-    timeline = list(service.get_user_timeline(created_after=recent_day))
+    print("----------")
+    recent = datetime.now() - timedelta(days=7)
+    recent_tz = to_utc(recent)
+    verbose = True
+    timeline = list(service.get_user_timeline(created_after=recent_tz, verbose=verbose))
     print(len(timeline))
-    #pprint(timeline[0])
 
-    #parsed_status = parse_status(timeline[0])
-    #pprint(parsed_status)
+    print("----------")
+    pprint(timeline[0])
+    print("----------")
+    parsed_status = service.parse_status(timeline[0])
+    pprint(parsed_status)
 
-    records = []
-    for status in timeline:
-        print("...", status["id"])
-        records.append(parse_status(status))
+    print("----------")
+    records = [service.parse_status(status) for status in timeline]
     print(len(records))
 
-    breakpoint()
+    print("----------")
+    df = DataFrame(records)
+    print(df.head())
+
+    print("----------")
+    print("EXPORT TO CSV...")
+    csv_filepath = os.path.join(EXPORTS_DIR, "recent_posts.csv")
+    print(csv_filepath)
+    df.to_csv(csv_filepath, index=False)
+
+    print("----------")
+    print("EXPORT TO SQLITE...")
+    db = Database()
+    db.insert_df(df=df, table_name="recent_posts")
